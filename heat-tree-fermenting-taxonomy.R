3f2ed641e94a362c0166828bb5e3203b864aa930
Xin (David) Zhao <davidzhao1015@gmail.com>
2025-08-19 21:14:59 -0600

    updated code chunk to include abundance data successfully

--- heat-tree-fermenting-taxonomy.R
+++ heat-tree-fermenting-taxonomy.R
@@ -8,9 +8,8 @@ taxonomy_data <- read.csv("taxonomy_table.csv")
 # Inspect first few rows of the data
 head(taxonomy_data)
 
-
 # Count unique Species_label
-length(unique(taxonomy_data$Species_label)) # 36 spp.
+# length(unique(taxonomy_data$Species_label)) # 36 spp.
 
 
 
@@ -31,57 +30,45 @@ count_data3$Species_label <- rownames(count_data3)
 rownames(count_data3) <- NULL
 head(count_data3)
 
-length(unique(count_data3$Species_label)) # 36 spp.
+# length(unique(count_data3$Species_label)) # 36 spp.
+
+
+# Normalize freq data into relative proportions
+count_data3$normalized_prop <- count_data3$"Fermented foods1" / 115
+
+range(count_data3$normalized_prop)
 
 # Rename Fermented foods1 to count
 colnames(count_data3)[1] <- "fermented_food_freq"
 
 # Merge taxonomy data into count data by the column Species_label
 count_data4 <- merge(count_data3, taxonomy_data, by = "Species_label", all.x = TRUE)
-
 print(count_data4)
 
 # Show column names
-colnames(count_data4)
+# colnames(count_data4)
 
 
 
 # --- Parse taxonomy data ---
-obj <- parse_tax_data(count_data4, class_cols = 3:8, named_by_rank = TRUE)
+obj <- parse_tax_data(count_data4, class_cols = 4:9, named_by_rank = TRUE)
 
 class(obj)
 print(obj)
 
 
-
-
-
-
-# obj$data$tax_data
-
-# obj$data$tax_occ <- obj$data$tax_data[,c(1,3)]
-# print(obj$data$tax_occ)
-
-# length(unique(obj$data$tax_occ$taxon_id)) # 36 
-
-
-# print(obj$data$tax_occ$fermented_food_freq)
-
-# named_vec <- setNames(obj$data$tax_occ$fermented_food_freq, obj$data$tax_occ$taxon_id)
-# print(named_vec)
-
-
-# print(obj$n_obs())
-# obj$taxon_names()
-# obj$data$tax_occ$taxon_id
-
+# Get per-taxon information
+obj$data$tax_abund <- calc_taxon_abund(obj, "tax_data", cols = "normalized_prop")
+print(obj)
 
 
 # --- Draw heat tree ---
+set.seed(123)
+
 ht_plot_abund <- heat_tree(obj,
     node_label = obj$taxon_names(),
-    node_size = named_vec,
-    # node_color = named_vec,
+    node_size = obj$data$tax_abund$normalized_prop,
+    node_color = obj$data$tax_abund$normalized_prop,
     node_color_range = c("yellow", "green", "cyan"),
     initial_layout = "reingold-tilford",
     layout = "davidson-harel",
@@ -89,6 +76,8 @@ ht_plot_abund <- heat_tree(obj,
 )
 
 ht_plot_abund
+ggsave("heat_tree_plot_abund.pdf", plot = ht_plot_abund, width = 12, height = 8)
+
 
 
 
@@ -99,14 +88,14 @@ ht_plot_abund
 
 
 
-tax_data <- parse_tax_data(
-    taxonomy_data, 
-    class_cols = 2:7,
-    named_by_rank = TRUE
-)
+# tax_data <- parse_tax_data(
+#     taxonomy_data, 
+#     class_cols = 2:7,
+#     named_by_rank = TRUE
+# )
 
-class(tax_data)
-print(tax_data)
+# class(tax_data)
+# print(tax_data)
 
 # --- Draw heat tree
 # ht_plot <- heat_tree(
@@ -128,7 +117,7 @@ print(tax_data)
 # )
 
 
-print(tax_data$data$tax_data$count)
+# print(tax_data$data$tax_data$count)
 
 # --- Simple heat tree ---
 set.seed(123)
